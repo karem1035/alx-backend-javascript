ea516d06ee9db62b0183bdb54a9e6c019e9b6bff
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-06-28 15:17:26 +0300

    task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(
-    new Error(`${filename} cannot be processed`),
-  );
+  return new Promise((r, e) => {
+    e(new Error(`${filename} cannot be processed`));
+  });
 }
