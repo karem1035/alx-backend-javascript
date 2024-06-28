b2688346617cde05685a8aed395344f23cb5ecb4
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-06-28 13:22:35 +0300

    task 3 error fix 2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,9 +3,9 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then((resolveArray) => {
-      let text = `${resolveArray[0].body} ${resolveArray[1].firstName} ${resolveArray[1].lastName}`;
-
-      console.log(text);
+      console.log(
+        `${resolveArray[0].body} ${resolveArray[1].firstName} ${resolveArray[1].lastName}`
+      );
     })
     .catch(() => console.log('Signup system offline'));
 }
