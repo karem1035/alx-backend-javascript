e35cb25b405bfab8cc726ce06dbf77b9b3a5f094
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-06-28 13:13:21 +0300

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,9 +1,13 @@
 import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((resolveArray) => {
-    let text = `${resolveArray[0].body} ${resolveArray[1].firstName} ${resolveArray[1].lastName}`;
+  Promise.all([uploadPhoto(), createUser()])
+    .then((resolveArray) => {
+      let text = `${resolveArray[0].body} ${resolveArray[1].firstName} ${resolveArray[1].lastName}`;
 
-    console.log(text);
-  });
+      console.log(text);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
 }
