947ee6da14bf6fe53d8725821852f08113498620
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-03 05:13:24 +0300

    updates

--- 0x03-ES6_data_manipulation/4-main.js
+++ 0x03-ES6_data_manipulation/4-main.js
@@ -5,11 +5,11 @@ console.log(
   updateStudentGradeByCity(getListStudents(), 'San Francisco', [
     { studentId: 5, grade: 97 },
     { studentId: 1, grade: 86 },
-  ])
+  ]),
 );
 
 console.log(
   updateStudentGradeByCity(getListStudents(), 'San Francisco', [
     { studentId: 5, grade: 97 },
-  ])
+  ]),
 );
