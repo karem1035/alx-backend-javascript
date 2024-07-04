bd4ac0b5072c0766221c34d5db5f22b27c536202
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-04 18:05:10 +0300

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (!startString) return ''; // Return empty string if startString is falsy
-
-  const array = Array.from(set);
-  const sslength = startString.length;
-
-  // Use map to transform elements that start with startString
-  const cleanedArray = array.map((el) => {
-    if (el.startsWith(startString)) {
-      return el.substring(sslength);
-    }
-    return el; // Return original element if no change needed
-  });
-
-  // Join cleanedArray with '-' and return the result
-  return cleanedArray.join('-');
+  let result = '';
+  if (startString) {
+    set.forEach((el) => {
+      if (el.startsWith(startString)) {
+        result += (result ? '-' : '') + el.slice(startString.length);
+      }
+    });
+  }
+  return result;
 }
