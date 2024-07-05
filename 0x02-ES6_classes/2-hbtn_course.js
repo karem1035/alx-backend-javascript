37dd41592f957353ff1a49832421f9f56346ac9b
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-05 12:58:43 +0300

    removing underscore from the attr

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,9 +1,9 @@
 export default class HolbertonCourse {
   // Constructor function
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    this.name = name;
+    this.length = length;
+    this.students = students;
   }
 
   get name() {
