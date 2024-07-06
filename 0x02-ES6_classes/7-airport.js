da9e2ec7aff0c2713ac8d35f13cd11d696ddf9e8
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-06 19:40:03 +0300

    task 7 update

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,26 +1,26 @@
 export default class Airport {
   constructor(name, code) {
-    this.name = name;
-    this.code = code;
+    if (typeof name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
+
+    if (typeof code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    this._name = name;
+    this._code = code;
   }
 
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    this._name = value;
-  }
-
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    this._code = value;
-  }
-
-  get [Symbol.toStringTag]() {
-    return this._code;
+  toString() {
+    return `[object ${this._code}]`;
   }
 }
