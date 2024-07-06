8d3613920a506b80d50b75ca494ddca3f71037a7
Karem Darwesh <38433851+karem1035@users.noreply.github.com>
2024-07-06 17:54:33 +0300

    task 5 eslint

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -7,7 +7,7 @@ export default class Building {
     if (this.constrcuor !== Building) {
       if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          'Class extending Building must override evacuationWarningMessage'
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
